9831d40e55e94e9bfca142922e351658d8ef56d6
santospage <alessandro.siga@gmail.com>
2021-07-30 16:04:04 -0300

    add category service

---
+++ src/app/pages/categories/shared/category.service.ts
@@ -0,0 +1,76 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { errorHandler } from '@angular/platform-browser/src/browser';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators';
+
+import { Category } from './category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  private apiPath = '/api/categories';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Category[]> {
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDatatoCategories)
+    );
+  }
+
+  getById(id: number): Observable<Category> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDatatoCategory)
+    );
+  }
+
+  create(category: Category): Observable<Category> {
+    return this.http.post(this.apiPath, category).pipe(
+      catchError(this.handleError),
+      map(this.jsonDatatoCategory)
+    );
+  }
+
+  update(category: Category): Observable<Category> {
+    const url = `${this.apiPath}/${category.id}`;
+
+    return this.http.put(url, category).pipe(
+      catchError(this.handleError),
+      map(() => category)
+    );
+  }
+
+  delete(id: number): Observable<Category> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
+
+  // Private Methods
+
+  private jsonDatatoCategories(jsonData: any[]): Category[] {
+    const categories: Category[] = [];
+    jsonData.forEach(element => categories.push(element as Category));
+    return categories;
+  }
+
+  private jsonDatatoCategory(jsonData: any): Category {
+    return jsonData as Category;
+  }
+
+  private handleError(error: any): Observable<any> {
+    console.log('Erro na requisição ->', error);
+    return throwError(error);
+  }
+}
